7fb302796aa6c48601f51d4b9b1f91052a033e2f
Thomas Durand <thomas.durand.verjat@gmail.com>
2020-12-05 08:54:01 +0100

    Solved Day 5

--- AdventOfCode-Swift/Days/Day05/Day05.swift
+++ AdventOfCode-Swift/Days/Day05/Day05.swift
@@ -3,11 +3,40 @@ import Foundation
 
 enum Day05: Day {
     static func test() throws {
-        // TODO: add tests with preconditions
+        precondition(try! seatId(for: "BFFFBBFRRR") == 567)
+        precondition(try! seatId(for: "FFFBBBFRRR") == 119)
+        precondition(try! seatId(for: "BBFFBBFRLL") == 820)
     }
 
     static func run(input: String) throws {
-        // TODO: Implement Day 5
-        print("Day 5 is not yet implemented")
+        let seatsIds = Set(try input.components(separatedBy: .newlines).map { try seatId(for: $0) })
+
+        // Day 5-1
+        print("Solution for day 5-1 is \(seatsIds.max()!)")
+
+        // Day 5-2
+        print("Solution for day 5-1 is \(try findMissing(seatsIds: seatsIds))")
+    }
+
+    static func seatId(for code: String) throws -> Int {
+        // Convert to binary. B <=> 1 ; F <=> 0 ; R <=> 1 ; L <=> 0
+        let binary = code
+            .replacingOccurrences(of: "F", with: "0")
+            .replacingOccurrences(of: "B", with: "1")
+            .replacingOccurrences(of: "L", with: "0")
+            .replacingOccurrences(of: "R", with: "1")
+        guard let integer = Int(binary, radix: 2) else {
+            throw Errors.unparsable
+        }
+        return integer
+    }
+
+    static func findMissing(seatsIds: Set<Int>) throws -> Int {
+        for seat in seatsIds.min()!..<seatsIds.max()! {
+            if !seatsIds.contains(seat) && seatsIds.contains(seat - 1) && seatsIds.contains(seat + 1) {
+                return seat
+            }
+        }
+        throw Errors.unsolvable
     }
 }
--- README.md
+++ README.md
@@ -28,5 +28,6 @@ Some special attention will be addressed regarding performances, I'll try to sol
 | **2**  | 0,033s          | 580        | 611        |
 | **3**  | 0,004s          | 254        | 1666768320 |
 | **4**  | 0,021s          | 239        | 188        |
+| **5**  | 0,013s          | 989        | 548        |
 
 \* : Best elapsed time of the complete solution using my MacBook Pro 13in mid 2013 in "Release" mode
